180d2f7cd5e4e444e7f24aabd2a24832ac4d23d6
xenohunter <xenohunter@yandex.ru>
2017-08-24 16:07:57 +0300

    Added argument checks to converting function

--- src/utils/convert.ts
+++ src/utils/convert.ts
@@ -3,22 +3,38 @@ import converters from '../libs/converters';
 
 export default {
 
-    booleanToBytes(input): number[] {
+    booleanToBytes(input: boolean): number[] {
+
+        if (typeof input !== 'boolean') {
+            throw new Error('Boolean input is expected');
+        }
+
         return input ? [1] : [0];
+
     },
 
-    bytesToByteArrayWithSize(input): number[] {
+    bytesToByteArrayWithSize(input: Array<number> | Uint8Array): number[] {
+
+        if (!(input instanceof Array || input instanceof Uint8Array)) {
+            throw new Error('Byte array or Uint8Array input is expected');
+        } else if (input instanceof Array && !(input.every((n) => typeof n === 'number'))) {
+            throw new Error('Byte array contains non-numeric elements');
+        }
 
         if (!(input instanceof Array)) {
             input = Array.prototype.slice.call(input);
         }
 
         const lengthBytes = converters.int16ToBytes(input.length, true);
-        return [...lengthBytes, ...input];
+        return [...lengthBytes, ...input as Array<number>];
 
     },
 
-    longToByteArray(input): number[] {
+    longToByteArray(input: number): number[] {
+
+        if (typeof input !== 'number') {
+            throw new Error('Numeric input is expected');
+        }
 
         const bytes = new Array(7);
         for (let k = 7; k >= 0; k--) {
@@ -30,11 +46,21 @@ export default {
 
     },
 
-    stringToByteArray(input): number[] {
+    stringToByteArray(input: string): number[] {
+
+        if (typeof input !== 'string') {
+            throw new Error('String input is expected');
+        }
+
         return converters.stringToByteArray(input);
+
     },
 
-    stringToByteArrayWithSize(input): number[] {
+    stringToByteArrayWithSize(input: string): number[] {
+
+        if (typeof input !== 'string') {
+            throw new Error('String input is expected');
+        }
 
         const stringBytes = converters.stringToByteArray(input);
         const lengthBytes = converters.int16ToBytes(stringBytes.length, true);
