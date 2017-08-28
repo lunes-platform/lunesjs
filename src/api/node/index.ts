b3eb217a3a19665e95ffb4693bc72ec3f28a83ea
xenohunter <xenohunter@yandex.ru>
2017-08-28 22:09:27 +0300

    Added API requests for /transactions/*

--- src/api/node/index.ts
+++ src/api/node/index.ts
@@ -3,7 +3,11 @@ import v1Transactions from './v1/transactions';
 
 export interface INodeAPIv1 {
     transactions: {
+        get(id: string): Promise<any>;
         getList(address: string): Promise<any>;
+        utxSize(): Promise<any>;
+        utxGet(id: string): Promise<any>;
+        utxGetList(): Promise<any>;
     }
 }
 
--- src/api/node/v1/transactions.ts
+++ src/api/node/v1/transactions.ts
@@ -6,8 +6,24 @@ const fetch = createFetchWrapper(PRODUCTS.NODE, VERSIONS.V1, processJSON);
 
 export default {
 
+    get(id: string) {
+        return fetch(`/transactions/info/${id}`);
+    },
+
     getList(address: string, limit: number = 50) {
         return fetch(`/transactions/address/${address}/limit/${limit}`);
+    },
+
+    utxSize() {
+        return fetch('/transactions/unconfirmed/size');
+    },
+
+    utxGet(id: string) {
+        return fetch(`/transactions/unconfirmed/info/${id}`);
+    },
+
+    utxGetList() {
+        return fetch('/transactions/unconfirmed');
     }
 
 };
