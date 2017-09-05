0d9be0b5b4456353d8a8cbf548993ff8c0900929
Tsigel <tsddaniil@gmail.com>
2017-09-05 10:42:02 +0300

    Configure build system (add partial build without ts-utils and ts-api-validator)

--- src/libs/fetch.ts
+++ src/libs/fetch.ts
@@ -1,13 +1,25 @@
-declare const process: any;
-declare const require: any;
+declare let exports: any;
+declare let module: any;
+declare let require: any;
+declare let global: any;
 
 let fetchSubstitute;
-
-// Check for process.pid to prevent Browserify from tricking us
-if (typeof process !== 'undefined' && typeof process.pid === 'number') {
-    fetchSubstitute = require('node-fetch');
-} else {
-    fetchSubstitute = self.fetch.bind(self);
-}
+(function () {
+    if (typeof exports === 'object' && typeof module !== 'undefined') {
+        module.exports = require('node-fetch');
+    } else {
+        var g;
+        if (typeof window !== 'undefined') {
+            g = window
+        } else if (typeof global !== 'undefined') {
+            g = global
+        } else if (typeof self !== 'undefined') {
+            g = self
+        } else {
+            g = this
+        }
+        fetchSubstitute = g.fetch.bind(g);
+    }
+})();
 
 export default fetchSubstitute;
