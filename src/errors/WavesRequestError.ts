6fe7def71b2add4621a05debd676b1bf0e8b2560
xenohunter <xenohunter@yandex.ru>
2017-10-11 19:18:51 +0300

    Customized fetch error details

--- src/errors/WavesRequestError.ts
+++ src/errors/WavesRequestError.ts
@@ -1,10 +1,25 @@
 import WavesError from './WavesError';
 
 
+const FAILED_TO_FETCH = 'Failed to fetch';
+
+
+function normalizeErrorData(data) {
+    if (!data.error && data.message && data.message.indexOf(FAILED_TO_FETCH) !== -1) {
+        return {
+            error: -1,
+            message: 'failed to fetch'
+        };
+    } else {
+        return data;
+    }
+}
+
+
 export default class WavesRequestError extends WavesError {
 
     constructor(url, data) {
-        super(`Server request to '${url}' has failed`, data);
+        super(`Server request to '${url}' has failed`, normalizeErrorData(data));
         this.name = 'WavesRequestError';
     }
 
