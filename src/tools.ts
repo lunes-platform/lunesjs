ecb5d3ada57582582d78b8ed76ce1ba0bb518eeb
xenohunter <xenohunter@yandex.ru>
2017-11-19 11:56:44 +0300

    Exposed Base58 converters

--- src/tools.ts
+++ src/tools.ts
@@ -7,6 +7,11 @@ export default {
     getAddressFromPublicKey(publicKey: string) {
         const publicKeyBytes = base58.decode(publicKey);
         return crypto.buildRawAddress(publicKeyBytes);
+    },
+
+    base58: {
+        encode: base58.encode,
+        decode: base58.decode
     }
 
 };
