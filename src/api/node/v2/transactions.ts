0d9f1267c64d102fc48031ad5ebf420dbdcea72c
xenohunter <xenohunter@yandex.ru>
2017-11-22 20:15:41 +0300

    Reverted API v2 transactions.get() method behavior Added transactions.utxGet() method

--- src/api/node/v2/transactions.ts
+++ src/api/node/v2/transactions.ts
@@ -3,25 +3,32 @@ import { siftTransaction } from '../../schemaTools';
 import { CANCEL_LEASING_TX_NAME } from '../../../constants';
 
 
+function extendTransaction(transaction) {
+    if (transaction.transactionType === CANCEL_LEASING_TX_NAME) {
+        return v1Transactions.get(transaction.leaseTransactionId)
+            .then(siftTransaction)
+            .then((leaseTransaction) => {
+                transaction.leaseTransactionAmount = leaseTransaction.amount;
+                return transaction;
+            });
+    } else {
+        return transaction;
+    }
+}
+
+
 export default {
 
     get(id: string) {
         return v1Transactions.get(id)
-            // TODO : add this UTX-fallback behavior to wavesplatform/swagger-api-design
-            .catch(() => v1Transactions.utxGet(id))
             .then(siftTransaction)
-            .then((transaction) => {
-                if (transaction.transactionType === CANCEL_LEASING_TX_NAME) {
-                    return v1Transactions.get(transaction.leaseTransactionId)
-                        .then(siftTransaction)
-                        .then((leaseTransaction) => {
-                            transaction.leaseTransactionAmount = leaseTransaction.amount;
-                            return transaction;
-                        });
-                } else {
-                    return transaction;
-                }
-            });
+            .then(extendTransaction);
+    },
+
+    utxGet(id: string) {
+        return v1Transactions.utxGet(id)
+            .then(siftTransaction)
+            .then(extendTransaction);
     }
 
 };
