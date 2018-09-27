d120a78599db861ddc000151f39c51a06adff8e8
wandyer <wandyer.s@gmail.com>
2018-09-27 10:59:38 -0300

    Fixing alises URL

--- src/api/node/aliases.ts
+++ src/api/node/aliases.ts
@@ -7,11 +7,11 @@ const fetch = createFetchWrapper(PRODUCTS.NODE, VERSIONS.V1, processJSON);
 export default {
 
     byAlias(alias: string) {
-        return fetch(`/address/alias/by-alias/${alias}`);
+        return fetch(`/addresses/alias/by-alias/${alias}`);
     },
 
     byAddress(address: string) {
-        return fetch(`/address/alias/by-address/${address}`);
+        return fetch(`/addresses/alias/by-address/${address}`);
     }
 
 };
