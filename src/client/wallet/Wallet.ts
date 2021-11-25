041c2a1f6829118772f109070421612026fc1eea
Lucas Oliveira <lucas.oliveira@protonmail.com>
2021-11-24 23:40:05 -0300

    added: Account of wallet

--- src/client/wallet/Wallet.ts
+++ src/client/wallet/Wallet.ts
@@ -1,1 +1,9 @@
-// Add Wallet
+type Account = {
+    seed?: string,
+    nonce?: number,
+    privateKey?: string,
+    publicKey?: string,
+    network?: "mainnet" | "testnet",
+    network_id?: "1" | "0",
+    n_words?: number,
+}
