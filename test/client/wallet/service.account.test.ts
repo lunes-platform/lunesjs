b2280d49e74bb5d15ef05816e16a1f95d91982b1
Lucas Oliveira <lucas.oliveira@lunes.io>
2022-02-06 16:25:33 -0300

    added: test for account from seed

--- test/client/wallet/service.account.test.ts
+++ test/client/wallet/service.account.test.ts
@@ -39,3 +39,64 @@ describe("Create Account From New Seed", () => {
         ).toEqual(true)
      })
 })
+
+
+describe("Create Account From Seed", () => {
+    const mainnetAccountFromSeed = (nonce?: number) => {
+        const seed = "scrub guard swim catch range upon dawn ensure segment alpha sentence spend effort bar benefit"
+        return new Account({
+            seed: seed,
+            nonce: nonce != undefined ? nonce : 0,
+            chain: WalletTypes.Chain.Mainnet
+        })
+    }
+
+    const testnetAccountFromSeed = (nonce?: number) => {
+        const seed = "scrub guard swim catch range upon dawn ensure segment alpha sentence spend effort bar benefit"
+        return new Account({
+            seed: seed,
+            nonce: nonce != undefined ? nonce : 0,
+            chain: WalletTypes.Chain.Testnet
+        })
+    }
+
+    it("Create Mainnet Account from seed", () => {
+        const addr = "37o7aY3eZZTXmzrDa5e4Wj3Z4ZZuyV42Aaj"
+        expect(mainnetAccountFromSeed().address)
+        .toEqual(addr)
+    })
+
+    it("Create Testnet Account from seed", () => {
+        const addr = "37PmyYwMGrH4uBR5V4DjCEvHGw4f2pdXW5u"
+        expect(mainnetAccountFromSeed().address)
+        .toEqual(addr)
+    })
+
+    const mainnetNonce12345Address: [number, string][] = [
+        [0, "37o7aY3eZZTXmzrDa5e4Wj3Z4ZZuyV42Aaj"],
+        [1, "37tD32367v1fiWgW8waw3QTdYTKKGrCV3zw"],
+        [2, "37qYK5eRJEr8a38hUXmxYv9aoQ8NpXH7Aqd"],
+        [3, "37w8stLd9JQwUKBrBUQr1VryJuhS3RWqEen"],
+        [4, "37vVbQVXEE4Lvs7X4wimsoxAvqBmoyHsWDJ"],
+    ]
+    test.each(mainnetNonce12345Address)("Create Mainnet Account with range of nonces", (nonce, address) =>{
+        expect(
+            mainnetAccountFromSeed(nonce).address
+        ).toEqual(address)
+    })
+
+    const testnetNonce12345Address: [number, string][] =  [
+        [0, "37PmyYwMGrH4uBR5V4DjCEvHGw4f2pdXW5u"],
+        [1, "37UsS2vnqCqCqhFN3vAbivLMkpp4L8GMCyP"],
+        [2, "37SCi6Y81XffhDhZPWMdES2K1md7skK1mFu"],
+        [3, "37XoGuEKrbEUbVki6SzWh1jhXHCB6jnKFxS"],
+        [4, "37X9zRPDwWst43gNyvJSZKpu9CgWsHt1U8i"],
+    ]
+
+    test.each(testnetNonce12345Address)("Create Testnet Account with range of nonces", (nonce, address) =>{
+        expect(
+            testnetAccountFromSeed(nonce).address
+        ).toEqual(address)
+    })
+})
+
