ea1397abe974930691acca7d583afd0dc7035632
Lucas Oliveira <lucas.oliveira@lunes.io>
2022-02-06 16:25:13 -0300

    added: test for account with new seed

--- test/client/wallet/service.account.test.ts
+++ test/client/wallet/service.account.test.ts
@@ -2,37 +2,39 @@ import { Account } from "../../../src/client/wallet/service.account"
 import { WalletTypes } from "../../../src/client/wallet/wallet.types"
 import lunesCrypto from "../../../src/utils/crypto"
 
-function newMainnetAccount() {
-    return new Account({
-        chain: WalletTypes.Chain.Mainnet
-    })
-}
 
-function newTestnetAccount() {
-    return new Account({
-        chain: WalletTypes.Chain.Testnet
-    })
-}
 describe("Create Account From New Seed", () => {
+    const mainnetAccountFromNewSeed = () => {
+        return new Account({
+            chain: WalletTypes.Chain.Mainnet
+        })
+    }
+
+    const testnetAccountFromNewSeed = () => {
+        return new Account({
+            chain: WalletTypes.Chain.Testnet
+        })
+    }
+
     it("Test type of Mainnet Account from New Seed", () => {
-       expect(newMainnetAccount()).toBeInstanceOf(Account)
+       expect(mainnetAccountFromNewSeed()).toBeInstanceOf(Account)
     })
 
     it("Test type of Testnet Account from New Seed", () => {
-       expect(newTestnetAccount()).toBeInstanceOf(Account)
+       expect(testnetAccountFromNewSeed()).toBeInstanceOf(Account)
     })
 
     it("Test Address of Mainnet Account from New Seed", () => {
         expect(
             lunesCrypto.validateAddress(
-                newTestnetAccount().address, WalletTypes.Chain.Mainnet
+                mainnetAccountFromNewSeed().address, WalletTypes.Chain.Mainnet
             )
         ).toEqual(true)
      })
     it("Test Address of Testnet Account from New Seed", () => {
         expect(
             lunesCrypto.validateAddress(
-                newTestnetAccount().address, WalletTypes.Chain.Testnet
+                testnetAccountFromNewSeed().address, WalletTypes.Chain.Testnet
             )
         ).toEqual(true)
      })
