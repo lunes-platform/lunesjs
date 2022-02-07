39995c083c96db61ad1126343798a0071d348d56
Robson <robson.miranda@lunes.io>
2022-02-07 21:22:57 +0000

    added: created class transaction 💸
    Implemented class constructor, types and interface
    
    #6

--- src/client/transactions/transfer/service.transfer.ts
+++ src/client/transactions/transfer/service.transfer.ts
@@ -1,1 +1,42 @@
-export class TransferToken {}
+import { WalletTypes } from "../../wallet/wallet.types"
+import { BaseTransaction, TransferTransaction } from "../BaseTransaction"
+
+export class TransferToken implements BaseTransaction {
+    sender?: WalletTypes.PublicKey
+    receiver?: WalletTypes.Address
+    chain?: WalletTypes.Chain.Mainnet | WalletTypes.Chain.Testnet
+    amount?: number
+    assetFee?: string
+    assetId?: string
+    timestamp?: number
+    fee?: number
+    privateKey?: WalletTypes.PrivateKey
+
+    constructor(obj: TransferTransaction) {
+        this.sender = obj.sender
+        this.receiver = obj.receiver
+        this.chain = obj.chain
+        this.amount = obj.amount
+        this.assetFee = obj.assetFee
+        this.assetId = obj.assetId
+        this.timestamp = obj.timestamp
+        this.fee = obj.fee
+        this.privateKey = obj.privateKey
+    }
+
+    transaction(): object {
+        return {}
+    }
+
+    sign(privateKey: WalletTypes.PrivateKey): object {
+        return {}
+    }
+
+    ready(): boolean {
+        return false
+    }
+
+    send(): object {
+        return {}
+    }
+}
