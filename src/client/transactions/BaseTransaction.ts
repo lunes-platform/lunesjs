38979e1e9378b914ee0be13a9a2bbd24528620cd
Robson <robson.miranda@lunes.io>
2022-02-07 21:18:38 +0000

    added: transfer transaction types
    #6

--- src/client/transactions/BaseTransaction.ts
+++ src/client/transactions/BaseTransaction.ts
@@ -1,6 +1,21 @@
+import { WalletTypes } from "../wallet/wallet.types"
+
 export interface BaseTransaction {
+    constructor: object
     transaction(): object
-    sign(): object
+    sign(privateKey: WalletTypes.PrivateKey): object
     ready(): boolean
     send(): object
+}
+
+export type TransferTransaction = {
+    sender?: WalletTypes.PublicKey
+    receiver?: WalletTypes.Address
+    chain?: WalletTypes.Chain.Mainnet | WalletTypes.Chain.Testnet
+    amount?: number
+    assetFee?: string
+    assetId?: string
+    timestamp?: number
+    fee?: number
+    privateKey?: WalletTypes.PrivateKey
 }
