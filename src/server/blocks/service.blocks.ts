72b139f75d7a7e7ad8cae19963ab938dd33a81a7
Lucas Oliveira <lucas.oliveira@lunes.io>
2022-03-23 12:04:53 -0300

    added: based interfaces block

---
+++ src/server/blocks/service.blocks.ts
@@ -0,0 +1,12 @@
+import { IBlock } from "./block.types"
+
+
+/*
+* This function get `height` e return a full block
+*/
+function blockByHeight(height: number): IBlock {
+    return fetch(
+        "url",
+        "GET"
+    ).json()
+}
