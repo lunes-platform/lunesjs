f151ab09a970e33782aaed91a5bc5c9d97b7e9fd
Lucas Oliveira <lucas.oliveira@lunes.io>
2022-03-27 03:54:29 +0000

    refactored: wallet with pattern factory

--- src/client/wallet/service.account.ts
+++ src/client/wallet/service.account.ts
@@ -1,22 +1,74 @@
 import { IAccount, WalletTypes } from "./wallet.types"
-import generate from "./utils"
+import cryptoUtils from "../../utils/crypto"
 
-export class Account implements IAccount {
-    nWords: WalletTypes.nWords
-    chain: WalletTypes.Chain
-    nonce: WalletTypes.Nonce
-    seed: WalletTypes.Seed
-    privateKey: WalletTypes.PrivateKey
-    publicKey: WalletTypes.PublicKey
-    address: WalletTypes.Address
+class Account implements IAccount {
+    privateKey: string
+    publicKey: string
+    address: string
+    chain: number
+    nonce: number
+    seed: string
 
-    constructor(account: IAccount) {
-        const wallet: IAccount = generate.wallet(account)
-        this.chain = wallet.chain
-        this.nonce = wallet.nonce
-        this.seed = wallet.seed
+    constructor(wallet: IAccount) {
         this.privateKey = wallet.privateKey
         this.publicKey = wallet.publicKey
         this.address = wallet.address
+        this.chain = wallet.chain
+        this.nonce = wallet.nonce
+        this.seed = wallet.seed
+    }
+}
+
+export function accountFactory({
+    privateKey,
+    publicKey,
+    address,
+    nWords,
+    chain,
+    nonce,
+    seed
+}: {
+    privateKey?: string
+    publicKey?: string
+    address?: string
+    nWords?: number
+    chain?: number
+    nonce?: number
+    seed?: string
+} = {}): Account {
+    if (seed != undefined) {
+        return new Account({
+            ...cryptoUtils.fromExistingSeed(
+                seed,
+                nonce != undefined ? nonce : 0,
+                chain != undefined ? chain : 0
+            )
+        })
+    } else if (privateKey != undefined) {
+        return new Account({
+            ...cryptoUtils.fromPrivateKey(
+                privateKey,
+                chain != undefined ? chain : 0
+            )
+        })
+    } else if (publicKey != undefined) {
+        return new Account({
+            ...cryptoUtils.fromPublicKey(
+                publicKey,
+                chain != undefined ? chain : 0
+            )
+        })
+    } else if (address != undefined) {
+        return new Account({
+            ...cryptoUtils.fromAddress(address, chain != undefined ? chain : 0)
+        })
+    } else {
+        return new Account({
+            ...cryptoUtils.fromNewSeed(
+                nWords != undefined ? nWords : 12,
+                nonce != undefined ? nonce : 0,
+                chain != undefined ? chain : 1
+            )
+        })
     }
 }
--- src/utils/crypto.ts
+++ src/utils/crypto.ts
@@ -9,15 +9,9 @@ const cryptoUtils = {
         chain: WalletTypes.Chain
     ): IAccount => {
         const hidden_seed = wasm.hiddenSeed(nonce, seed)
-        const privateKey = wasm.toPrivateKeyHex(
-            wasm.fromStrHex(hidden_seed)
-        )
+        const privateKey = wasm.toPrivateKeyHex(wasm.fromStrHex(hidden_seed))
         const publicKey = wasm.toPublicKeyHex(wasm.fromStrHex(privateKey))
-        const address = wasm.toAddressHex(
-            1,
-            chain,
-            wasm.fromStrHex(publicKey)
-        )
+        const address = wasm.toAddressHex(1, chain, wasm.fromStrHex(publicKey))
 
         return {
             nonce: nonce,
@@ -33,13 +27,11 @@ const cryptoUtils = {
         chain: WalletTypes.Chain
     ): IAccount => {
         const publicKey = wasm.toPublicKeyHex(wasm.b58ToVec(privateKey))
-        const address = wasm.toAddressHex(
-            1,
-            chain,
-            wasm.fromStrHex(publicKey)
-        )
+        const address = wasm.toAddressHex(1, chain, wasm.fromStrHex(publicKey))
 
         return {
+            seed: "",
+            nonce: 0,
             chain: chain,
             privateKey: privateKey,
             publicKey: wasm.hexToB58(publicKey),
@@ -47,13 +39,12 @@ const cryptoUtils = {
         }
     },
     fromPublicKey: (publicKey: string, chain: WalletTypes.Chain): IAccount => {
-        const address = wasm.toAddressHex(
-            1,
-            chain,
-            wasm.b58ToVec(publicKey)
-        )
+        const address = wasm.toAddressHex(1, chain, wasm.b58ToVec(publicKey))
 
         return {
+            seed: "",
+            nonce: 0,
+            privateKey: "",
             chain: chain,
             publicKey: publicKey,
             address: wasm.hexToB58(address)
@@ -61,6 +52,10 @@ const cryptoUtils = {
     },
     fromAddress: (address: string, chain: WalletTypes.Chain): IAccount => {
         return {
+            seed: "",
+            nonce: 0,
+            privateKey: "",
+            publicKey: "",
             chain: chain,
             address: address
         }
@@ -80,29 +75,38 @@ const cryptoUtils = {
         return cryptoUtils.fromExistingSeed(seed.join(" "), nonce, chain)
     },
     validateAddress: (address: string, chain: WalletTypes.Chain): boolean => {
-        return wasm.validateAddress(
-            chain,
-            wasm.b58ToVec(address)
-        )
+        return wasm.validateAddress(chain, wasm.b58ToVec(address))
     },
-    validateSignature: (publicKey: string, message: string, signature: string): boolean => {
+    validateSignature: (
+        publicKey: string,
+        message: string,
+        signature: string
+    ): boolean => {
         return wasm.validateSignature(
             wasm.toVecu32(wasm.b58ToVec(publicKey)),
-            wasm.stringToVecu32(message),
+            wasm.toVecu32(wasm.b58ToVec(message)),
             wasm.toVecu32(wasm.b58ToVec(signature))
         )
     },
     fastSignature: (privateKey: string, message: string) => {
-        return wasm.hexToB58(wasm.vecu32ToHex(wasm.fastSignature(
-            wasm.toVecu32(wasm.b58ToVec(privateKey)),
-            wasm.stringToVecu32(message)
-        )))
+        return wasm.hexToB58(
+            wasm.vecu32ToHex(
+                wasm.fastSignature(
+                    wasm.toVecu32(wasm.b58ToVec(privateKey)),
+                    wasm.toVecu32(wasm.b58ToVec(message))
+                )
+            )
+        )
     },
     fullSignature: (privateKey: string, message: string) => {
-        return wasm.hexToB58(wasm.vecu32ToHex(wasm.fullSignature(
-            wasm.toVecu32(wasm.b58ToVec(privateKey)),
-            wasm.stringToVecu32(message)
-        )))
+        return wasm.hexToB58(
+            wasm.vecu32ToHex(
+                wasm.fullSignature(
+                    wasm.toVecu32(wasm.b58ToVec(privateKey)),
+                    wasm.toVecu32(wasm.b58ToVec(message))
+                )
+            )
+        )
     }
 }
 
