3087f0f65256519d57d07fdb05d46be20922122b
Lucas Oliveira <lucas.oliveira@lunes.io>
2022-03-27 03:54:58 +0000

    added: transfer transaction

--- src/client/transactions/BaseTransaction.ts
+++ src/client/transactions/BaseTransaction.ts
@@ -1,21 +1,7 @@
 import { WalletTypes } from "../wallet/wallet.types"
 
 export interface BaseTransaction {
-    constructor: object
-    transaction(): object
     sign(privateKey: WalletTypes.PrivateKey): object
-    ready(): boolean
+    transaction(): object
     send(): object
-}
-
-export type TransferTransaction = {
-    sender?: WalletTypes.PublicKey
-    receiver?: WalletTypes.Address
-    chain?: WalletTypes.Chain.Mainnet | WalletTypes.Chain.Testnet
-    amount?: number
-    assetFee?: string
-    assetId?: string
-    timestamp?: number
-    fee?: number
-    privateKey?: WalletTypes.PrivateKey
 }
--- src/client/transactions/transfer/service.transfer.ts
+++ src/client/transactions/transfer/service.transfer.ts
@@ -1,42 +1,64 @@
+import { TransactionsTypes } from "../transactions.types"
 import { WalletTypes } from "../../wallet/wallet.types"
-import { BaseTransaction, TransferTransaction } from "../BaseTransaction"
+import { BaseTransaction } from "../BaseTransaction"
+import { ITransfer } from "./transfer.types"
+import validator from "./validator"
+import * as wasm from "lunesrs"
 
-export class TransferToken implements BaseTransaction {
-    sender?: WalletTypes.PublicKey
-    receiver?: WalletTypes.Address
-    chain?: WalletTypes.Chain.Mainnet | WalletTypes.Chain.Testnet
-    amount?: number
-    assetFee?: string
-    assetId?: string
-    timestamp?: number
+class TransferToken implements BaseTransaction {
+    private tx: ITransfer
+    private chain: WalletTypes.Chain
+    constructor(tx: ITransfer, chain: WalletTypes.Chain) {
+        this.tx = tx
+        this.chain = chain
+    }
+
+    transaction(): ITransfer {
+        return this.tx
+    }
+
+    sign(privateKey: WalletTypes.PrivateKey): ITransfer {
+        this.tx.signature = validator.sign(privateKey, this.tx)
+        return this.tx
+    }
+
+    async send() {
+        validator.send(this.tx)
+    }
+}
+
+export function transferTokenFactory(
+    senderPublicKey: string,
+    recipient: string,
+    amount: number,
+    chain?: WalletTypes.Chain,
+    timestamp?: number,
+    feeAsset?: string,
+    assetId?: string,
     fee?: number
-    privateKey?: WalletTypes.PrivateKey
-
-    constructor(obj: TransferTransaction) {
-        this.sender = obj.sender
-        this.receiver = obj.receiver
-        this.chain = obj.chain
-        this.amount = obj.amount
-        this.assetFee = obj.assetFee
-        this.assetId = obj.assetId
-        this.timestamp = obj.timestamp
-        this.fee = obj.fee
-        this.privateKey = obj.privateKey
+): TransferToken {
+    const chain_id = chain != undefined ? chain : WalletTypes.Chain.Mainnet
+    if (
+        false == validator.ready(senderPublicKey, recipient, amount, chain_id)
+    ) {
+        throw new Error("dados invalidos")
     }
-
-    transaction(): object {
-        return {}
-    }
-
-    sign(privateKey: WalletTypes.PrivateKey): object {
-        return {}
-    }
-
-    ready(): boolean {
-        return false
-    }
-
-    send(): object {
-        return {}
-    }
+    return new TransferToken(
+        {
+            senderPublicKey: senderPublicKey,
+            recipient: recipient,
+            amount: amount,
+            sender: wasm.hexToB58(
+                wasm.toAddressHex(1, chain_id, wasm.b58ToVec(senderPublicKey))
+            ),
+            timestamp:
+                timestamp != undefined ? timestamp : new Date().getTime(),
+            feeAsset: feeAsset != undefined ? feeAsset : "",
+            assetId: assetId != undefined ? assetId : "",
+            type: TransactionsTypes.TransferToken.int,
+            fee: fee != undefined ? fee : TransactionsTypes.TransferToken.fee,
+            signature: ""
+        },
+        chain_id
+    )
 }
