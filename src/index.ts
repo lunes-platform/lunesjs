f84555d9a65488a79d111830979c86f780557ce5
Lucas Oliveira <lucas.oliveira@lunes.io>
2022-04-05 10:57:27 -0300

    update: deps version

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export { transferTokenFactory } from "./client/transactions/transfer/service.transfer"
 export { accountFactory } from "./client/wallet/service.account"
 export { IAccount } from "./client/wallet/wallet.types"
-import  cryptoUtils from "./utils/crypto"
+import cryptoUtils from "./utils/crypto"
 export default cryptoUtils
