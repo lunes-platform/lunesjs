3359a113b06a2e1f456c380f6869fb3dcf673bb3
nunesinc <nunes@nunesinc.net>
2022-04-17 17:52:42 -0300

    add: test for validateAddr

--- test/blockchain/address/address.service.test.ts
+++ test/blockchain/address/address.service.test.ts
@@ -3,6 +3,6 @@ import { validateAddr } from "../../../src/blockchain/address/service.address"
 describe("validateAddr function- suite test block service", () => {
     it("validateAddr - validate address, passing true addrress", async () => {
         const result = await validateAddr("37pU1WLv8vettr9UHGfPpTMWe1twne8FCBn")
-        expect(result).toStrictEqual("a")
+        expect(result).toStrictEqual({"address": "37pU1WLv8vettr9UHGfPpTMWe1twne8FCBn", "valid": true})
     })
 })
