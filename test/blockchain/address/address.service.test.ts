4235db0457dd74a60b4b063db785596e992f4e2c
nunesinc <nunes@nunesinc.net>
2022-04-17 17:03:26 -0300

    add: test case skeleton

---
+++ test/blockchain/address/address.service.test.ts
@@ -0,0 +1,8 @@
+import { validateAddr } from "../../../src/blockchain/address/service.address"
+
+describe("validateAddr function- suite test block service", () => {
+    it("validateAddr - validate address, passing true addrress", async () => {
+        const result = await validateAddr("37pU1WLv8vettr9UHGfPpTMWe1twne8FCBn")
+        expect(result).toStrictEqual("a")
+    })
+})
