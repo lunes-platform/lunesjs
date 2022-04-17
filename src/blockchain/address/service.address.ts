f106429260645dfbd4f8993250d11a087d4862b7
nunesinc <nunes@nunesinc.net>
2022-04-17 17:04:17 -0300

    add: yarn fmt

--- src/blockchain/address/service.address.ts
+++ src/blockchain/address/service.address.ts
@@ -1,4 +1,33 @@
-
 import axios from "axios"
 
-fsfs
+const BASEURL = "https://lunesnode.lunes.io/addresses/"
+
+/*
+ * This function get wallet accounts addresses
+ * --- validation: {from} {to}
+ *
+ */
+
+/*
+ * This function check whether address {address} is valid or not
+ * --- validation: {address} : string
+ *
+ */
+
+export async function validateAddr(address: String): Promise<any> {
+    const url = `${BASEURL}validate/${address}`
+
+    const response = await axios.get(url)
+    return response.data
+}
+
+/*
+ * This function check whether address {address} is valid or not
+ * --- validation: {address} : string
+ * 
+
+
+export async function validateAddr(address:String):  Promise<any>  {
+    
+}
+ */
