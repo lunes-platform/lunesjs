d65bc32428ba8e0f7440a9670f9ffa6acc77bfae
Lucas Oliveira <olivmath@protonmail.com>
2022-05-17 23:26:31 -0300

    add: issue token

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,13 @@ import { withdrawStakeFactory } from "./tx/stake/withdrawStake.service"
 import { createStakeFactory } from "./tx/stake/createStake.service"
 import { walletFactory } from "./wallet/wallet.service"
 import { crypto } from "./utils/crypto"
+import { issueTokenFactory } from "./tx/tokens/issueToken.service"
 
 export default {
     transferTokenFactory,
     withdrawStakeFactory,
     createStakeFactory,
+    issueTokenFactory,
     walletFactory,
     crypto
 }
---
+++ src/tx/tokens/issueToken.service.ts
@@ -0,0 +1,112 @@
+import { serializeIssueToken } from "./utils"
+import broadcast from "../../utils/broadcast"
+import signer from "../../utils/signer"
+
+export class IssueToken {
+    senderPublicKey: string
+    description: string
+    reissuable: number
+    timestamp: number
+    signature: string
+    quantity: number
+    decimals: number
+    message: string
+    name: string
+    type: number
+    fee: number
+    constructor(
+        senderPublicKey: string,
+        description: string,
+        reissuable: number,
+        timestamp: number,
+        quantity: number,
+        decimals: number,
+        name: string,
+        fee: number
+    ) {
+        this.senderPublicKey = senderPublicKey
+        this.description = description
+        this.reissuable = reissuable
+        this.timestamp = timestamp
+        this.quantity = quantity
+        this.decimals = decimals
+        this.signature = ""
+        this.message = ""
+        this.name = name
+        this.fee = fee
+        this.type = 3
+    }
+
+    transaction() {
+        return {
+            senderPublicKey: this.senderPublicKey,
+            description: this.description,
+            reissuable: this.reissuable,
+            timestamp: this.timestamp,
+            signature: this.signature,
+            quantity: this.quantity,
+            decimals: this.decimals,
+            name: this.name,
+            type: this.type,
+            fee: this.fee
+        }
+    }
+
+    sign(privateKey: string): IssueToken {
+        ;[this.signature, this.message] = signer<IssueToken>(
+            privateKey,
+            serializeIssueToken,
+            this
+        )
+        return this
+    }
+
+    async broadcast(node?: string) {
+        return await broadcast(
+            node != undefined ? node : "https://lunesnode.lunes.io",
+            this.transaction()
+        )
+    }
+}
+
+export type Issue = {
+    senderPublicKey: string
+    description: string
+    reissuable?: boolean
+    timestamp?: number
+    quantity: number
+    decimals?: number
+    name: string
+    fee?: number
+}
+
+export function issueTokenFactory(tx: Issue): IssueToken {
+    const timestamp = tx.timestamp != undefined ? tx.timestamp : Date.now()
+    const decimals = tx.decimals != undefined ? tx.decimals : 8
+    const fee = tx.fee != undefined ? tx.fee : 100000
+    const reissuable = () => {
+        if (tx.reissuable != undefined) {
+            return tx.reissuable == true ? 1 : 0
+        } else {
+            return 1
+        }
+    }
+
+    if (tx.description.length > 1000) throw new Error(`Description should be less than 1000, but ${tx.description.length}`)
+    if (timestamp < 1483228800) throw new Error(`Timestamp should be greater than 1483228800, but ${timestamp}`)
+    if (tx.name.length > 16) throw new Error(`Name should be less than 16 char, but ${tx.name.length}`)
+    if (tx.quantity <= 0) throw new Error(`Amount should be greater than 0, but ${tx.quantity}`)
+    if (decimals > 8) throw new Error(`Decimals should be less than 8, but ${decimals}`)
+    if (fee < 100000) throw new Error(`Fee should be greater than 100000, but ${fee}`)
+
+    return new IssueToken(
+        tx.senderPublicKey,
+        tx.description,
+        reissuable(),
+        timestamp,
+        Math.floor(tx.quantity * 10e7),
+        decimals,
+        tx.name,
+        fee
+    )
+}
